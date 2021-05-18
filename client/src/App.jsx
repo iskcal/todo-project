67bdffb2d0cb361c609e529343b7329937055746
iskcal <cross_icon@outlook.com>
2021-05-18 16:16:08 +0800

    add todo list component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-import { VStack, Heading } from '@chakra-ui/react'
+import { VStack, Heading } from '@chakra-ui/react';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <VStack spacing={4} p={4}>
       <Heading as="h1" m={2} fontWeight="bold" fontSize="4xl">Todo List</Heading>
+      <TodoList />
     </VStack>
   );
 }
